87f4227698b922c5e2593bd95f315b39277b88d0
jstarman <jstarman@incomm.com>
2015-12-10 10:03:43 -0800

    more samples and added a client

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,12 +11,33 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("====================");
+            Console.WriteLine("Starting Round Robin");
+            var tasks = new MakeRequest().StartRoundRobin();
+            Task.WaitAll(tasks);
+            foreach(var task in tasks)
+            {
+                Console.WriteLine(task.Result);
+            }
+
+            Console.WriteLine("Ending Round Robin");
+            Console.WriteLine("====================");
         }
     }
 
     public class MakeRequest
     {
+        public Task<string>[] StartRoundRobin()
+        {
+            return new[] { Get("http://localhost:8000"),
+                            Get("http://localhost:8000/error"),
+                            Get("http://localhost:8000/info"),
+                            Get("http://localhost:8000/error/important"),
+                            Get("http://localhost:8000/error/service"),
+                            Get("http://localhost:8000/error/web")
+            };
+        }
+
         public async Task<string> Get(string uri)
         {
             var httpClient = new HttpClient();
--- OwinApp/HomeModule.cs
+++ OwinApp/HomeModule.cs
@@ -1,11 +1,13 @@
 ﻿using Nancy;
 using Nancy.Owin;
+using Nancy.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Linq;
 using log4net;
+using System.Threading.Tasks;
 
 namespace OwinApp
 {
@@ -16,14 +18,14 @@ namespace OwinApp
         public HomeModule(ILog logger)
         {
             _logger = logger;
-            Get["/"] = x =>
+            Get["/"] = _ =>
             {
                 _logger.Info("Hello, is anyone listening.");
                 _logger.Error("A bad bug", new Exception("Just kidding it's not too bad."));
                 return "Logging Error and Info" + DateTime.Now.ToString();
             };
 
-            Get["/error"] = x =>
+            Get["/error"] = _ =>
             {
                 new ImportantService(_logger).DoSomethingImportant();
                 new LongRunningProcess(_logger).StartProcess();
@@ -31,29 +33,64 @@ namespace OwinApp
                 return "Error logged" + DateTime.Now.ToString();
             };
 
-            Get["/info"] = x =>
+            Get["/info"] = _ =>
             {
                 _logger.Info("Hello, is anyone listening.");
                 return "Info logged" + DateTime.Now.ToString();
             };
 
-            Get["/error/important"] = x =>
+            Get["/error/important"] = _ =>
             {
                 new ImportantService(_logger).DoSomethingImportant();
                 return "Error ImportantService" + DateTime.Now.ToString();
             };
 
-            Get["/error/service"] = x =>
+            Get["/error/service"] = _ =>
             {
                 new LongRunningProcess(_logger).StartProcess();
                 return "Error LongRunningProcess" + DateTime.Now.ToString();
             };
 
-            Get["/error/web"] = x =>
+            Get["/error/web"] = _ =>
             {
                 new WebRequestClient(_logger).StartRequest();
                 return "Error WebRequestClient" + DateTime.Now.ToString();
             };
+
+            //capture query strings http://localhost:8000/custom?blah=123
+            Get["/custom"] = _ =>
+            {
+                return this.Request.Query["blah"];
+            };
+
+            //capture url properties http://localhost:8000/custom/Bob
+            Get["/custom/{name}"] = x =>
+            {
+                return x.name;
+            };
+
+            /* Request body
+             * { 
+                    "name" : "some name",
+                    "number" : 1,
+                    "date" : "2015-12-08T20:57:00.0000000-08:00",
+                    "list" : ["one", "two", "three" ]
+                }
+             */
+            Post["/sendjson", true] = async (x, ct) =>
+            {
+                await Task.Delay(500); //contrived async wait.
+                var sender = this.Bind<Sender>();
+                return Response.AsJson(sender);
+            };            
         }
+    }
+
+    public class Sender
+    {
+        public string name { get; set; }
+        public int number { get; set; }
+        public DateTime date { get; set; }
+        public IEnumerable<string> list { get; set; }
     }
 }
